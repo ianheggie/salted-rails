ec0800fdb00e9943beecf65d957e4a08a6435049
Ian Heggie <ian@heggie.biz>
2014-03-09 23:45:58 +1100

    Cleaun up init.sh

--- init.sh
+++ init.sh
@@ -4,14 +4,16 @@ msg='OK: Vagrant has been setup: ready for vagrant up [--provider=digital_ocean]
 
 if which VirtualBox; then
   echo Found Virtualbox
+  VirtualBox --help | line
 else
-  echo ACTION REQUIRED: Please install Virtualbox 4.2.18 or later from https://www.virtualbox.org/wiki/Downloads
+  echo 'ACTION REQUIRED: Please install Virtualbox 4.2.18 or later from https://www.virtualbox.org/wiki/Downloads'
   msg='(PLEASE RERUN SCRIPT AFTERWARDS)'
 fi
 
 if which vagrant; then
-  echo Found vagrant
-  echo Checking vagrant plugins are installed ...
+  echo 'Found vagrant:'
+  vagrant -v
+  echo 'Checking vagrant plugins are installed ...'
 
   vagrant plugin list > /tmp/t$$
   for plugin in deep_merge vagrant-digitalocean vagrant-vbguest salted-rails
@@ -24,16 +26,16 @@ if which vagrant; then
   done
 
 else
-  echo ACTION REQUIRED: Please install vagrant 1.3.3 (1.3.4 has a bug) from http://www.vagrantup.com/
+  echo 'ACTION REQUIRED: Please install vagrant 1.3.3 (1.3.4 has a bug) from http://www.vagrantup.com/'
   msg='(PLEASE RERUN SCRIPT AFTERWARDS)'
 fi
 
 if [ ! -f Vagrantfile ]; then
   if [ -f Vagrantfile.example ]; then
     if [ -f Vagrantfile ]; then
-      echo Found Vagrantfile '(previously copied from example)'
+      echo 'Found Vagrantfile (previously copied from example)'
     else
-      echo Copying Vagrantfile.example to Vagrantfile '(so you can customize it)'
+      echo 'Copying Vagrantfile.example to Vagrantfile (so you can customize it)'
       cp Vagrantfile.example Vagrantfile
     fi
   else
@@ -42,7 +44,7 @@ if [ ! -f Vagrantfile ]; then
   fi
 fi
 
-mkdir -p $HOME/.vagrant.d
+mkdir -p "$HOME/.vagrant.d"
 
 if [ -f "$HOME/.vagrant.d/Vagrantfile" ]; then
   echo Found global Vagrantfile
